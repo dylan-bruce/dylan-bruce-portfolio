5472ca9f07cc012c2e27a5590d1d81acebed21e1
Dylan Bruce <dyanb1127@gmail.com>
2022-05-19 14:28:09 -0400

    Added content to Resume page

--- src/layout/resume/Resume.jsx
+++ src/layout/resume/Resume.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import { Link } from 'react-router-dom';
 import dylanResume from './dylanResume.pdf';
