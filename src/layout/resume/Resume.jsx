ec0f599de43404b500d1965034f072124b0d7f27
Dylan Bruce <dyanb1127@gmail.com>
2022-05-19 14:24:18 -0400

    Added content to Resume page

--- src/layout/resume/Resume.jsx
+++ src/layout/resume/Resume.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Link } from 'react-router-dom';
+import dylanResume from './dylanResume.pdf';
 
 const Resume = () => {
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const resume = dylanResume;
+  
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
   return (
-    <div>Resume</div>
+    <div className='resume'>
+      <div className='text'>
+        <h3>Resume</h3>
+        <Link className='resumeDownload' to='./dylanResume.pdf' target='_blank' download>Download</Link>
+      </div>
+      <Document
+        file={resume}
+        onLoadSuccess={onDocumentLoadSuccess}
+        className='resumePdf'
+        >
+        <Page pageNumber={pageNumber} />
+      </Document>
+    </div>
   )
 }
 
