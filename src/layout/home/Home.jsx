c2cae4e96f7512cf34f08934302a4f4f396be119
Dylan Bruce <dyanb1127@gmail.com>
2022-05-21 23:05:42 -0400

    Adding alt text for home image

--- src/layout/home/Home.jsx
+++ src/layout/home/Home.jsx
@@ -4,7 +4,7 @@ import me from './photo/me.png'
 const Home = () => {
   return (
     <div className='about-me'>
-      <img className='me' src={me}></img>
+      <img className='me' src={me} alt="Dylan Bruce in front of a green backdrop"></img>
       <hr/>
       <h3 className='about-me-title'>About Me</h3>
       <hr/>
@@ -19,6 +19,8 @@ const Home = () => {
         I finished out my degree and I started to self teach C# because I was stuck on the idea of creating my own video game using Unity.  After following some tutorials and
         one of Unity's guided lessons on making a small functional game, I decided that I wanted to pursue this idea more directly.
         This is when I enrolled in Thinkful's full-stack software engineering boot camp that I have finished on 5/9/2022.  
+      </p>
+      <p>
         In this boot camp, I learned about web requests, front-end development, back-end development, and data structures and algorithms. 
         Utilizing these skills that I had learned, I created front-end, back-end, and full-stack CRUD projects during the boot camp.  
       </p>
